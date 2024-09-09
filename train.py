eab4df503bdb1ccd7114e4a717d161a37e10d136
jcairel <jcaireljr@comcast.net>
2024-09-09 01:22:56 +0000

    Update with new results

--- train.py
+++ train.py
@@ -1,13 +1,16 @@
-import pandas as pd 
+import pandas as pd
+
 drug_df = pd.read_csv("Data/drug200.csv")
 drug_df = drug_df.sample(frac=1)
 drug_df.head(3)
 
 from sklearn.model_selection import train_test_split
+
 X = drug_df.drop("Drug", axis=1).values
 y = drug_df.Drug.values
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=125)
+    X, y, test_size=0.3, random_state=125
+)
 
 # Create Pipeline
 from sklearn.compose import ColumnTransformer
@@ -19,12 +22,20 @@ from sklearn.preprocessing import OrdinalEncoder, StandardScaler
 cat_col = [1, 2, 3]
 num_col = [0, 4]
 
-transform = ColumnTransformer([("encoder", OrdinalEncoder(), cat_col), 
-("num_imputer", SimpleImputer(strategy="median"), num_col),
-("num_scaler", StandardScaler(), num_col),])
+transform = ColumnTransformer(
+    [
+        ("encoder", OrdinalEncoder(), cat_col),
+        ("num_imputer", SimpleImputer(strategy="median"), num_col),
+        ("num_scaler", StandardScaler(), num_col),
+    ]
+)
 
-pipe = Pipeline(steps=[("preprocessing", transform),
-("model", RandomForestClassifier(n_estimators=100, random_state=125)),])
+pipe = Pipeline(
+    steps=[
+        ("preprocessing", transform),
+        ("model", RandomForestClassifier(n_estimators=100, random_state=125)),
+    ]
+)
 pipe.fit(X_train, y_train)
 
 # Evaluate model
@@ -36,18 +47,22 @@ f1 = f1_score(y_test, predictions, average="macro")
 
 print("Accuracy:", str(round(accuracy, 2) * 100) + "%", "F1:", round(f1, 2))
 
-with open("Results/metrics.txt", 'w') as outfile:
+with open("Results/metrics.txt", "w") as outfile:
     outfile.write(f"\nAccuracy = {round(accuracy, 2)}, F1 Score = {round(f1, 2)}.")
 
 import matplotlib.pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+
 cm = confusion_matrix(y_test, predictions, labels=pipe.classes_)
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=pipe.classes_)
 disp.plot()
 plt.savefig("Results/model_results.png", dpi=120)
 
 
-import skops.io as sio 
+import skops.io as sio
 
 sio.dump(pipe, "Model/drug_pipeline.skops")
-sio.load("Model/drug_pipeline.skops", trusted=sio.get_untrusted_types(file="Model/drug_pipeline.skops"))
+sio.load(
+    "Model/drug_pipeline.skops",
+    trusted=sio.get_untrusted_types(file="Model/drug_pipeline.skops"),
+)
